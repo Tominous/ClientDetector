7915b5d778e226feaf648bc3c9b3468dffcdab56
Tominous <18189490+Tominous@users.noreply.github.com>
2021-10-01 22:42:49 +0100

    Update ClientDisabler.java

--- src/main/java/de/sportkanone123/clientdetector/spigot/command/impl/ClientDisabler.java
+++ src/main/java/de/sportkanone123/clientdetector/spigot/command/impl/ClientDisabler.java
@@ -23,7 +23,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 public class ClientDisabler {
-    /*public static boolean handle(CommandSender sender, Command command, String label, String[] args) {
+    public static boolean handle(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 3 && Bukkit.getPlayer(args[1]) != null){
             if(args[2].equalsIgnoreCase("aristois")){
                 de.sportkanone123.clientdetector.spigot.clientdisabler.ClientDisabler.checkAristois(sender, Bukkit.getPlayer(args[1]));
@@ -33,5 +33,5 @@ public class ClientDisabler {
         }
 
         return false;
-    }*/
+    }
 }
